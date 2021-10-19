4fd821288e42c617786c0f2e7440b259c3b03a37
tirupathiAmz <tirupathi.mandali@amzur.com>
2021-10-19 20:27:15 +0530

    New Alert

---
+++ CustomAlertview/CustomAlertView.swift
@@ -0,0 +1,90 @@
+//
+//  CustomAlertView.swift
+//  HelpZRequester
+//
+//  Created by sriharshay on 24/03/20.
+//  Copyright © 2020 harsha. All rights reserved.
+//
+
+import UIKit
+
+public class CustomAlertView: UIView {
+    
+    @IBOutlet public weak var okButton: UIButton!
+    @IBOutlet public weak var cancelButton: UIButton!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var alertBaseView: UIView!
+    @IBOutlet weak var buttonsStack: UIStackView!
+    
+    private let nibName = String(describing: CustomAlertView.self)
+    private var contentView: UIView?
+    
+    // Caallbacks
+    var okClicked: (()->())?
+    var cancelClicked: (()->())?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupUI()
+    }
+    
+    private func setupUI() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        view.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        self.addSubview(view)
+        contentView = view
+        
+        self.icon.layer.cornerRadius = self.icon.bounds.size.width / 2
+        self.icon.layer.borderWidth = 5.0
+        self.icon.layer.borderColor = UIColor.white.cgColor
+        alertBaseView.layer.cornerRadius = 10.0
+    }
+    
+    private func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+   public func showAlert(alertType: AlertType, title: String?, message: String?) {
+        if title == nil {self.title.isHidden = true} else {self.title.isHidden = false}
+        if message == nil {self.message.isHidden = true} else {self.message.isHidden = false}
+        self.title.text = title
+        self.message.text = message
+        switch alertType {
+        case .success: self.icon.image = UIImage(named: "ic_alert_success")
+        case .error: self.icon.image = UIImage(named: "ic_alert_error")
+        case .info: self.icon.image = UIImage(named: "ic_alert_info")
+        }
+        guard let contentView = self.contentView else {return}
+        UIApplication.shared.keyWindow?.addSubview(contentView)
+    }
+    
+    @IBAction func cancelClicked(_ sender: Any) {
+        self.contentView?.removeFromSuperview()
+        self.cancelClicked?()
+    }
+    
+    @IBAction func okClicked(_ sender: Any) {
+        self.contentView?.removeFromSuperview()
+        self.okClicked?()
+    }
+}
+
+public enum AlertType {
+    case success
+    case info
+    case error
+}
--- Example/newPodsLib/ViewController.swift
+++ Example/newPodsLib/ViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 import newPodsLib
 class ViewController: UIViewController {
+    
+    private lazy var alertView: CustomAlertView = {
+        let alertView = CustomAlertView()
+        alertView.cancelButton.isHidden = true
+        return alertView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        MyClass.ShowMessage()
+//        MyClass.ShowMessage(controller: se, title: <#String#>, message: <#String#>)
+        let button = UIButton(frame: CGRect(x: 50, y: 50, width: 50, height: 50))
+        button.setTitle("Tap Me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .brown
+        button.addTarget(self, action: #selector(showMe), for: .touchUpInside)
+        
+        self.view.addSubview(button)
     }
 
+    @objc private func showMe(){
+        alertView.showAlert(alertType: .info, title: "Info", message: "This is my new Alert")
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
