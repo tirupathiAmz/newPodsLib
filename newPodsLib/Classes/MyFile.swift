f91d901c3dfee05478ccb533cd45ff6bc8b92171
tirupathiAmz <tirupathi.mandali@amzur.com>
2021-10-19 15:50:55 +0530

    new commit

---
+++ newPodsLib/Classes/MyFile.swift
@@ -0,0 +1,14 @@
+//
+//  MyFile.swift
+//  newPodsLib
+//
+//  Created by Tirupathi Mandali on 10/19/21.
+//
+
+import Foundation
+public struct MyClass {
+    
+    public static func ShowMessage(){
+        print("I'm here")
+    }
+}
